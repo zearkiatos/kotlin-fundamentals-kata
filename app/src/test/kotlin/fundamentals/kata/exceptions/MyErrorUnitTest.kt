ca61d563f659862ad1417339f40547473ace47c3
zearkiatos <caprilespe@outlook.com>
2026-03-19 23:47:11 -0300

    Fixing test

--- app/src/test/kotlin/fundamentals/kata/exceptions/MyErrorUnitTest.kt
+++ app/src/test/kotlin/fundamentals/kata/exceptions/MyErrorUnitTest.kt
@@ -4,7 +4,6 @@ import java.io.ByteArrayOutputStream
 import java.io.PrintStream
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
-import fundamentals.kata.exceptions.executionSomeFunctionWithFinally
 
 class MyErrorUnitTest {
     @Test
