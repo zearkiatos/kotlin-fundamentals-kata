0c6d02b5d8ad0a340a267e242de597a87ed87b09
zearkiatos <caprilespe@outlook.com>
2026-04-16 18:32:09 -0400

    SetOf

--- app/src/test/kotlin/fundamentals/kata/lists/setOfUnitTest.kt
+++ app/src/test/kotlin/fundamentals/kata/lists/setOfUnitTest.kt
@@ -59,4 +59,42 @@ class SetOfUnitTest {
         assertTrue('A' in setList)
         assertTrue('B' in setList)
     }
+
+    @Test
+    fun `Given a setOf list Whet it is tour it with for loop Then it should return all elements`() {
+        val setList = setOf('A', 'B', 'C')
+        val expectedList = listOf('A', 'B', 'C')
+        var index = 0
+
+        for (element in setList) {
+            assertEquals(element, expectedList[index])
+            index++
+        }
+    }
+
+    @Test
+    fun `Given a mutable setOf list When it is called with add a new element Then it should add a new element`() {
+        val setList = mutableSetOf('A', 'B')
+        val expectedElement = 'C'
+
+        assertEquals(setList.size, 2)
+
+        setList.add(expectedElement)
+
+        assertEquals(setList.size, 3)
+        assertTrue(setList.contains(expectedElement))
+    }
+
+    @Test
+    fun `Given a mutable setOf list When it is called with remove a new element Then it should remove the element`() {
+        val setList = mutableSetOf('A', 'B')
+        val expectedElement = 'A'
+
+        assertEquals(setList.size, 2)
+
+        setList.remove(expectedElement)
+
+        assertEquals(setList.size, 1)
+        assertFalse(setList.contains(expectedElement))
+    }
 }
